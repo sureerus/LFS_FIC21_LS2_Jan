63504e3ed7e55524c4148d33cdd1d8432f0a713f
sureerus <jan.krueger05@gmail.com>
2023-01-16 12:30:18 +0100

    upload

---
+++ src/old/Treppe.java
@@ -0,0 +1,11 @@
+package old;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Treppe {
+    public static void main (String []args){
+
+
+    }
+}
---
+++ src/old/Wurzel.java
@@ -0,0 +1,8 @@
+package old;
+
+public class Wurzel {
+    public static void main(String[] args){
+        double erg = Math.sqrt(13);
+        System.out.println(erg);
+    }
+}
